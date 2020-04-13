d2238b9ae11974eb6eb1fa0a8435bed372445032
Zhark10 <arkady.zharavin@citronium.biz>
2020-04-14 00:44:36 +0300

    changed helper functions for date configs

--- src/utils/helpers/date.ts
+++ src/utils/helpers/date.ts
@@ -5,17 +5,16 @@ export const YEARS = Array(100)
   .map((_: any, i: number) => i + 1910);
 
 export const MONTHS = monthsShort();
-export const MONTHS_NUMBERS = [
-  '01',
-  '02',
-  '03',
-  '04',
-  '05',
-  '06',
-  '07',
-  '08',
-  '09',
-  '10',
-  '11',
-  '12',
-];
+
+function zeroFill(num: string | number, size: number) {
+  let s = num + '';
+  while (s.length < size) {
+    s = `0${s}`;
+  }
+  return s;
+}
+
+export const MONTHS_NUMBERS = [...Array(12)].map((_, i) => zeroFill(i + 1, 2));
+
+export const getDays = (number: number) =>
+  [...Array(number)].map((_, i) => zeroFill(i + 1, 2));
