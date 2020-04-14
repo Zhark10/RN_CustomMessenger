48867513ba2bbc77b270fd24dcbc24aa3f83adc2
Zhark10 <arkady.zharavin@citronium.biz>
2020-04-15 01:40:52 +0300

    styles for datepicker component

--- src/components/shared/picker/DatePicker.tsx
+++ src/components/shared/picker/DatePicker.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import {TDatePicker} from './T_DatePicker';
 import {Text, View} from 'react-native';
 import {DatePickerStyles} from './S_DatePicker';
+import ScrollPicker from '../../../libs/scroll-picker/scroll-picker';
 import {
   YEARS,
   MONTHS,
@@ -10,27 +11,23 @@ import {
   getDays,
 } from '../../../utils/helpers/date';
 import moment from 'moment';
-import ScrollPicker from '../../../libs/scroll-picker/scroll-picker';
 
 export const DatePicker: React.FC<TDatePicker> = ({
-  onSendDate,
+  onSaveDate,
   viewStyles: {answerFieldColor, buttonColor},
+  mode = 'bornDate',
 }) => {
-  const [monthNumber, setMonthNumber] = React.useState('01');
-  const [year, setYear] = React.useState('1914');
-  const [day, setDay] = React.useState(0);
+  const [month, setMonth] = React.useState('01');
+  const [year, setYear] = React.useState('1910');
+  const [day, setDay] = React.useState('01');
 
-  const [DAYS, setDAYS] = React.useState(getDays(31));
+  const daysInCurentMonth = moment(`${year}-${month}`, 'YYYY-MM').daysInMonth();
 
   React.useEffect(() => {
-    const daysInCurentMonth = moment(
-      `${year}-${monthNumber}`,
-      'YYYY-MM',
-    ).daysInMonth();
-    const days = getDays(daysInCurentMonth);
-    setDAYS(days);
-  }, [monthNumber, year]);
+    onSaveDate({month, year, day});
+  }, [month, year, day, onSaveDate]);
 
+  const DAYS = getDays(daysInCurentMonth);
   return (
     <View style={DatePickerStyles.main}>
       <View
@@ -40,65 +37,79 @@ export const DatePicker: React.FC<TDatePicker> = ({
           flexDirection: 'row',
           justifyContent: 'space-between',
         }}>
-        <ScrollPicker
-          dataSource={MONTHS}
-          selectedIndex={0}
-          itemHeight={40}
-          wrapperHeight={100}
-          wrapperColor={answerFieldColor}
-          highlightColor={buttonColor}
-          renderItem={(data: string, index: number, isSelected: boolean) => (
-            <Text
-              style={{
-                fontFamily: 'Circe-Regular',
-                fontSize: 20,
-                color: isSelected ? '#4F4E4E' : '#C3C3C3',
-              }}>
-              {data}
-            </Text>
-          )}
-          onValueChange={(month: string) =>
-            setMonthNumber(MONTHS_NUMBERS[MONTHS.indexOf(month)])
-          }
-        />
-        <ScrollPicker
-          dataSource={DAYS}
-          selectedIndex={0}
-          itemHeight={40}
-          wrapperHeight={100}
-          wrapperColor={answerFieldColor}
-          highlightColor={buttonColor}
-          renderItem={(data: string, index: number, isSelected: boolean) => (
-            <Text
-              style={{
-                fontFamily: 'Circe-Regular',
-                fontSize: 20,
-                color: isSelected ? '#4F4E4E' : '#C3C3C3',
-              }}>
-              {data}
-            </Text>
-          )}
-          onValueChange={setDay}
-        />
-        <ScrollPicker
-          dataSource={YEARS}
-          selectedIndex={0}
-          itemHeight={40}
-          wrapperHeight={100}
-          wrapperColor={answerFieldColor}
-          highlightColor={buttonColor}
-          renderItem={(data: string, index: number, isSelected: boolean) => (
-            <Text
-              style={{
-                fontFamily: 'Circe-Regular',
-                fontSize: 20,
-                color: isSelected ? '#4F4E4E' : '#C3C3C3',
-              }}>
-              {data}
-            </Text>
-          )}
-          onValueChange={setYear}
-        />
+        <View style={{width: mode === 'bornDate' ? '30%' : '46%'}}>
+          <ScrollPicker
+            dataSource={MONTHS}
+            selectedIndex={0}
+            itemHeight={40}
+            wrapperHeight={100}
+            wrapperColor={answerFieldColor}
+            highlightColor={buttonColor}
+            renderItem={(data: string, index: number, isSelected: boolean) => (
+              <Text
+                style={{
+                  fontFamily: 'Circe-Regular',
+                  fontSize: 20,
+                  color: isSelected ? '#4F4E4E' : '#C3C3C3',
+                }}>
+                {data}
+              </Text>
+            )}
+            onValueChange={(_month: string) =>
+              setMonth(MONTHS_NUMBERS[MONTHS.indexOf(_month)])
+            }
+          />
+        </View>
+        {mode === 'bornDate' ? (
+          <View style={{width: '30%'}}>
+            <ScrollPicker
+              dataSource={DAYS}
+              selectedIndex={0}
+              itemHeight={40}
+              wrapperHeight={100}
+              wrapperColor={answerFieldColor}
+              highlightColor={buttonColor}
+              renderItem={(
+                data: string,
+                index: number,
+                isSelected: boolean,
+              ) => (
+                <Text
+                  style={{
+                    fontFamily: 'Circe-Regular',
+                    fontSize: 20,
+                    color: isSelected ? '#4F4E4E' : '#C3C3C3',
+                  }}>
+                  {data}
+                </Text>
+              )}
+              onValueChange={setDay}
+            />
+          </View>
+        ) : (
+          <></>
+        )}
+        <View style={{width: mode === 'bornDate' ? '30%' : '46%'}}>
+          <ScrollPicker
+            dataSource={YEARS}
+            selectedIndex={0}
+            itemHeight={40}
+            wrapperHeight={100}
+            wrapperColor={answerFieldColor}
+            highlightColor={buttonColor}
+            renderItem={(data: string, index: number, isSelected: boolean) => (
+              <Text
+                style={{
+                  fontFamily: 'Circe-Regular',
+                  fontSize: 20,
+                  color: isSelected ? '#4F4E4E' : '#C3C3C3',
+                }}>
+                {data}
+              </Text>
+            )}
+            onValueChange={setYear}
+          />
+        </View>
       </View>
     </View>
   );
