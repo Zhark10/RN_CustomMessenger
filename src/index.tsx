03a2209044798f49b20acaaf276a16dc5480643d
Zhark10 <arkady.zharavin@citronium.biz>
2020-04-16 23:27:59 +0300

    changed initial screen

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,10 @@ import {isIos} from './utils/helpers/platform';
 import {getAnswerSize} from './utils/helpers/answer-panel-size-detect';
 
 import {useChatMiddleware} from './utils/hooks/USE_ChatMiddleware';
-import {useAnswerFieldAnimation} from './utils/hooks/USE_AnswerFieldAnimation';
+import {
+  useAnswerFieldAnimation,
+  useAdditionalAnswerFieldAnimation,
+} from './utils/hooks/USE_AnswerFieldAnimation';
 import {MainStyles} from './styles';
 import {
   ChatInput,
@@ -18,6 +21,9 @@ import {
   ChatButton,
   ChatPayment,
   ChatDatepicker,
+  ChatAddress,
+  ChatAddressAdditional,
+  ChatPaymentAdditional,
 } from './components/answers/exports';
 import MessagesField from './components/messages/MessagesField/MessagesField';
 
@@ -27,8 +33,19 @@ export const MessangerStack: FC<TLibraryInputData> = libraryInputData => {
     currentChatBotQuestion: {myAnswer},
     answerFieldVisible,
   } = chatMiddleware;
+  const myAnswerType = Object.getOwnPropertyNames(myAnswer)[0];
+  const isShowAdditionalPanel =
+    myAnswerType === EAnswerType.ADDRESS ||
+    myAnswerType === EAnswerType.PAYMENT;
 
-  const chatProps: TChatProps = {chatMiddleware, libraryInputData};
+  const additionalAnswerFieldAnimation = useAdditionalAnswerFieldAnimation(
+    answerFieldVisible,
+  );
+  const chatProps: TChatProps = {
+    chatMiddleware,
+    libraryInputData,
+    setVisibleAdditionalAnswerPanel: additionalAnswerFieldAnimation.setVisible,
+  };
   const answerSize = getAnswerSize(myAnswer);
   const answerFieldAnimation = useAnswerFieldAnimation(
     answerFieldVisible,
@@ -36,7 +53,6 @@ export const MessangerStack: FC<TLibraryInputData> = libraryInputData => {
   );
 
   const selectAnswerField = (): React.ReactNode => {
-    const myAnswerType = Object.getOwnPropertyNames(myAnswer)[0];
     const answerFields = {
       [EAnswerType.INPUT]: ChatInput,
       [EAnswerType.MULTICHOICE]: ChatMultichoice,
@@ -45,38 +61,71 @@ export const MessangerStack: FC<TLibraryInputData> = libraryInputData => {
       [EAnswerType.BUTTON]: ChatButton,
       [EAnswerType.PAYMENT]: ChatPayment,
       [EAnswerType.DATEPICKER]: ChatDatepicker,
+      [EAnswerType.ADDRESS]: ChatAddress,
     };
     const AnswerField = answerFields[myAnswerType];
     return <AnswerField {...chatProps} />;
   };
 
+  const selectAdditionalPanelForAnswer = (): React.ReactNode => {
+    const answerFields = {
+      [EAnswerType.PAYMENT]: ChatPaymentAdditional,
+      [EAnswerType.ADDRESS]: ChatAddressAdditional,
+    };
+    const AdditionalAnswerField = answerFields[myAnswerType];
+    return (
+      <AdditionalAnswerField
+        setVisibleAdditionalAnswerPanel={
+          additionalAnswerFieldAnimation.setVisible
+        }
+        {...chatProps}
+      />
+    );
+  };
+
   const {answerFieldColor, chatBackgroundColor} = libraryInputData.viewStyles;
 
   return (
-    <KeyboardAvoidingView
-      behavior={isIos ? 'padding' : undefined}
-      style={[
-        MainStyles.main,
-        {
-          backgroundColor: chatBackgroundColor,
-        },
-      ]}>
-      <MessagesField answerSize={answerSize} {...chatProps} />
-      {libraryInputData.chatHeaderComponent ? (
-        libraryInputData.chatHeaderComponent
+    <>
+      <KeyboardAvoidingView
+        behavior={isIos ? 'padding' : undefined}
+        style={[
+          MainStyles.main,
+          {
+            backgroundColor: chatBackgroundColor,
+          },
+        ]}>
+        <MessagesField answerSize={answerSize} {...chatProps} />
+        {libraryInputData.chatHeaderComponent ? (
+          libraryInputData.chatHeaderComponent
+        ) : (
+          <></>
+        )}
+        <Animated.View
+          style={[
+            MainStyles.animAnswerPanel,
+            {
+              height: answerFieldAnimation.offsetValue,
+              backgroundColor: answerFieldColor,
+            },
+          ]}>
+          {answerFieldVisible && selectAnswerField()}
+        </Animated.View>
+      </KeyboardAvoidingView>
+      {isShowAdditionalPanel ? (
+        <Animated.View
+          style={[
+            MainStyles.animAdditionalAnswerPanel,
+            {
+              right: additionalAnswerFieldAnimation.offsetValue,
+              backgroundColor: answerFieldColor,
+            },
+          ]}>
+          {answerFieldVisible && selectAdditionalPanelForAnswer()}
+        </Animated.View>
       ) : (
         <></>
       )}
-      <Animated.View
-        style={[
-          MainStyles.anim,
-          {
-            height: answerFieldAnimation.offsetValue,
-            backgroundColor: answerFieldColor,
-          },
-        ]}>
-        {answerFieldVisible && selectAnswerField()}
-      </Animated.View>
-    </KeyboardAvoidingView>
+    </>
   );
 };
